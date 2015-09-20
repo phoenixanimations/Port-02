858ea962b2500b3c0b641b4bb73db57f71b9067a
phoenixanimations <Getpeanuts@Tristans-MacBook-Pro.local>
2015-09-20 03:19:49 -0400

    Quick fix.

--- Assets/Scripts/Status/Heal.cs
+++ Assets/Scripts/Status/Heal.cs
@@ -2,17 +2,8 @@
 using System.Collections;
 
 public class Heal : Status {
-
-//	public int Damage = 0;
-	
 	protected override void Start ()
 	{
 		base.Start ();
-//		this.gameObject.GetComponent<Creature>().AddStatus += thisWorks;
-	}
-
-	private void thisWorks ()
-	{
-		//this.gameObject.GetComponent<Creature>().Bonus = Damage;
 	}
 }
