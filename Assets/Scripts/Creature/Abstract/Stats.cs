a23365251786404b0a8383b0d3e4bfdbe63fdff8
RVNJ <lectorthevector@gmail.com>
2015-09-23 02:59:37 -0400

    Tier things
    stuff

--- Assets/Scripts/Creature/Abstract/Stats.cs
+++ Assets/Scripts/Creature/Abstract/Stats.cs
@@ -84,57 +84,72 @@ public class Stats : BasicTile
  * String Description;
  * String Class;
  * String SubClass;
- * String Slot;
+ * String Primary_Slot;
+ * String Secondary_Slot;
+ * String Helmet_Slot;
+ * String Chest_Slot;
+ * String Legs_Slot;
+ * String Ammo_Slot;
+ * 
  * 
  * int ID;
  *
  *
  *
- * float Hitpoints = Tier ((Weapon.Hitpoints) + (Shield.Hitpoints) + (Helmet.Hitpoints) + (Chest.Hitpoints) + (Legs.Hitpoints) + Modify_Hitpoints_Level);
+ * float Hitpoints = ((Weapon.Hitpoints) + (Shield.Hitpoints) + (Helmet.Hitpoints) + (Chest.Hitpoints) + (Legs.Hitpoints) + TierArray(Modified_Hitpoints_Level));
  * 
  **********In a FUNCTION**********
- * float Primary_Accuracy = ((Primary.Accuracy + Modify_Class_Level) / Enemy.Evade + Modify_Class_Level) * 0.5;
- * float Secondary_Accuracy = ((Secondary.Accuracy + Modify_Melee_Level) / Enemy.Evade + Modify_Class_Level) * 0.5;
- * float Evade = Primary.Evade + Secondary.Evade + Modify_Class_Level;
+ * float Primary_Accuracy = Mathf.floor(0.5 * ((Primary.Accuracy + Modified_Class_Level + Accuracy_Bonus) / (Enemy.Evade + Modified_Class_Level)));
+ * float Secondary_Accuracy = Mathf.floor(0.5 * ((Secondary.Accuracy + Modified_Class_Level + Accuracy_Bonus) / (Enemy.Evade + Modified_Class_Level)));
+ * float Evade = Mathf.floor(Primary.Evade + Secondary.Evade + Modified_Class_Level + Evade_Bonus);
  **********In a FUNCTION**********
  * 
  * float True_Hitpoints_Level;
  * float True_Melee_Level;
  * float True_Magic_Level;
  * float True_Archery_Level;
+ * //float True_Demo_Level;
  * 
- * float Hitpoints_Level_Bonus;
- * 
- * float Modify_Hitpoints_Level = Tier(True_Hitpoints_Level + Hitpoints_Level_Bonus);
- * float Modify_Melee_Level = Tier(True_Melee_Level + Melee_Level_Bonus);
- * float Modify_Magic_Level = Tier(True_Magic_Level + Magic_Level_Bonus);
- * float Modify_Archery_Level = Tier(True_Archery_Level + Archery_Level_Bonus);
- * 
- * float Primary_Melee_Damage = Modify_Melee_Level + Primary.Melee;
- * float Secondary_Melee_Damage = Modify_Melee_Level + Secondary.Melee;
- * 
+ *  
  * [component]
  * 
- * if (DW) 
+ * if (DualWielding) {
  * Melee_Level_Bonus = -10;
- * Archery_Level_Bonus = -10;
  * Magic_Level_Bonus = -10;
+ * Archery_Level_Bonus = -10;
+ * Demo_Level_Bonus = -10;
+ * }
  * 
- * float Primary_Archery_Damage = Modify_Archery_Level + Weapon.Archery;
- * float Secondary_Archery_Damage = Modify_Archery_Level + Secondary.Melee;
+ * float Hitpoints_Level_Bonus = 50;
  * 
- * float Primary_Magic_Damage = Modify_Magic_Level + Weapon.Archery;
- * float Secondary_Magic_Damage = Modify_Magic_Level + Weapon.Archery;
+ * float Modified_Hitpoints_Level = (True_Hitpoints_Level + 50 + Hitpoints_Level_Bonus);
+ * float Modified_Melee_Level = (True_Melee_Level + Melee_Level_Bonus);
+ * float Modified_Magic_Level = (True_Magic_Level + Magic_Level_Bonus);
+ * float Modified_Archery_Level = (True_Archery_Level + Archery_Level_Bonus);
+ * //float Modified_Demo_Level = (True_Demo_Level + Demo_Level_Bonus);
  * 
- * int Defect_Chance = Weapon.Defect_Chance + Shield.Defect_Chance + Helmet.Defect_Chance + Chest.Defect_Chance + Legs.Defect_Chance;
+ * float Primary_Melee_Damage = TierArray(Modified_Melee_Level + Primary.Melee_Damage);
+ * float Secondary_Melee_Damage = TierArray(Modified_Melee_Level + Secondary.Melee_Damage);
+ * 
+ * float Primary_Magic_Damage = TierArray(Modified_Magic_Level + Primary.Magic_Damage);
+ * float Secondary_Magic_Damage = TierArray(Modified_Magic_Level + Secondary.Magic_Damage);
+ * 
+ * float Primary_Archery_Damage = TierArray(Modified_Archery_Level + Primary.Archery_Damage);
+ * float Secondary_Archery_Damage = TierArray(Modified_Archery_Level + Secondary.Archery_Damage);
+ * 
+ * //float Primary_Demo_Damage = TierArray(Modified_Demo_Level + Primary.Demo_Damage);
+ * //float Secondary_Demo_Damage = TierArray(Modified_Demo_Level + Secondary.Demo_Damage);
+ * 
+ * int Defect_Chance = (Primary.Defect_Chance + Secondary.Defect_Chance + Helmet.Defect_Chance + Chest.Defect_Chance + Legs.Defect_Chance + Ammo.Defect_Chance);
  * Delegate Defect;
  * 
- * int Passive_Chance = Weapon.Passive_Chance + Shield.Passive_Chance + Helmet.Passive_Chance + Chest.Passive_Chance + Legs.Passive_Chance;
+ * int Passive_Chance = (Primary.Passive_Chance + Secondary.Passive_Chance + Helmet.Passive_Chance + Chest.Passive_Chance + Legs.Passive_Chance + Ammo.Pasive_Chance);
  * Delegate Passive;
  * 
  * float Melee_Resistance;
  * float Magic_Resistance;
  * float Archery_Resistance;
+ * //float Demo_Resistance;
  * 
  * int Movement;
  * int Force;
@@ -154,13 +169,23 @@ public class Stats : BasicTile
  * float Accuracy;
  * float Evade;
  * 
- * float Modify_Melee_Tier = Tier + 5;
- * float Modify_Magic_Tier = Tier;
- * float Modify_Archery_Tier = Tier;
+ * float Modified_MeleeDamage_ClassMod = Tier + 5;
+ * float Modified_MeleeAccuracy_ClassMod = Tier + 0;
+ * float Modified_MeleeEvade_ClassMod = Tier + 0;
+ * float Modified_MagicDamage_ClassMod = Tier + 0;
+ * float Modified_MagicAccuracy_ClassMod = Tier + 0;
+ * float Modified_MagicEvade_ClassMod = Tier + 5;
+ * float Modified_ArcheryDamage_ClassMod = Tier + 0;
+ * float Modified_ArcheryAccuracy_ClassMod = Tier + 5;
+ * float Modified_ArcheryEvade_ClassMod = Tier + 0;
+ * //float Modified_DemoDamage_ClassMod = Tier + 0;
+ * //float Modified_DemoAccuracy_ClassMod = Tier + 0;
+ * //float Modified_DemoEvade_ClassMod = Tier + 0;
  * 
- * float Melee_Damage 
- * float Archery_Damage
- * float Magic_Damage
+ * float Melee_Damage;
+ * float Magic_Damage;
+ * float Archery_Damage;
+ * //float Demo_Damage;
  *
  * int Defect_Chance
  * Delegate Defect;
@@ -171,6 +196,7 @@ public class Stats : BasicTile
  * float Melee_Resistance;
  * float Magic_Resistance;
  * float Archery_Resistance;
+ * float Demo_Resistance;
  * 
  * int Movement;
  * int Force;
