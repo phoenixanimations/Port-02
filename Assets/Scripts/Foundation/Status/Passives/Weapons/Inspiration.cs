842ea98886abe7b34a48581ee9210c4acee73821
Tristan Beckley <tristanbeckley@yahoo.com>
2016-04-11 02:17:16 -0400

    Added some ideas in the notes.

--- Assets/Scripts/Foundation/Status/Passives/Weapons/Inspiration.cs
+++ Assets/Scripts/Foundation/Status/Passives/Weapons/Inspiration.cs
@@ -4,5 +4,13 @@ using System.Collections.Generic;
 
 public class Inspiration : Status_Foundation 
 {
+	public float Bonus_Accuracy_Damage_Percent;
+	public float Bonus_Accuracy;
+
 
 }
+
+/*
+10,20,30 % damage
+5,10,15 acc
+*/
