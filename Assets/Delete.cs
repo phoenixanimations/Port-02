582c8215f1e0c1524b139b7181e1f17b7059f4a4
RVNJ <lectorthevector@gmail.com>
2016-05-30 19:08:37 -0600

    Added New Archery items, Removed Magic Weapon Res
    Added Kunai:
    2x Accuracy, 0.25x Evade, 25% Crit Damage, 10% Crit Chance, Teleports
    to Enemy - Requires Subclassing of 'Thrown'
    Added Grapple:
    0 Damage, Infinity Accuracy, transports player to selected tile
    Removed 5 Melee, Magic & Archery Res from:
    Choas Wand, Kaoss Wand, Siphon Wand
    Removed 10 Melee, Magic & Archery Res from:
    Ray's Caster Staff

---
+++ Assets/Delete.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Delete : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		string url = "http://www.phoenixanimations.com/_server/PHP/stresstest/getstress.php"; //hope.php || tristans_ugh.php
+		WWW www = new WWW(url);
+		yield return www;
+		// 
+		//         // check for errors
+		if (www.error == null)
+		{
+			Debug.Log("Done: " + www.text);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
